821c6bb842dd8e17f1def41c306e90a8d8a19e2f
HIPPER52 <hipper52@gmail.com>
2025-04-21 13:45:40 +0300

    lesson-08: Basic algorithms and data structures

---
+++ lru/cache.go
@@ -0,0 +1,90 @@
+package lru
+
+type LruCache interface {
+	Put(key, value string)
+	Get(key string) (string, bool)
+}
+
+type Node struct {
+	key   string
+	value string
+	prev  *Node
+	next  *Node
+}
+
+type lruCache struct {
+	capacity int
+	cache    map[string]*Node
+	head     *Node
+	tail     *Node
+}
+
+func NewLruCache(capacity int) LruCache {
+	return &lruCache{
+		capacity: capacity,
+		cache:    make(map[string]*Node),
+	}
+}
+
+func (lc *lruCache) Put(key, value string) {
+	if node, exists := lc.cache[key]; exists {
+		node.value = value
+		lc.moveToFront(node)
+		return
+	}
+
+	newNode := &Node{
+		key:   key,
+		value: value,
+	}
+
+	if len(lc.cache) >= lc.capacity {
+		delete(lc.cache, lc.tail.key)
+		lc.removeNode(lc.tail)
+	}
+
+	lc.addToFront(newNode)
+	lc.cache[key] = newNode
+}
+
+func (lc *lruCache) Get(key string) (string, bool) {
+	if node, exists := lc.cache[key]; exists {
+		lc.moveToFront(node)
+		return node.value, true
+	}
+	return "", false
+}
+
+func (lc *lruCache) moveToFront(node *Node) {
+	lc.removeNode(node)
+	lc.addToFront(node)
+}
+
+func (lc *lruCache) removeNode(node *Node) {
+	if node.prev != nil {
+		node.prev.next = node.next
+	} else {
+		lc.head = node.next
+	}
+
+	if node.next != nil {
+		node.next.prev = node.prev
+	} else {
+		lc.tail = node.prev
+	}
+}
+
+func (lc *lruCache) addToFront(node *Node) {
+	node.prev = nil
+	node.next = lc.head
+
+	if lc.head != nil {
+		lc.head.prev = node
+	}
+
+	lc.head = node
+
+	if lc.tail == nil {
+		lc.tail = node
+	}
+}
