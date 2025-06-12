5531ac37f375855b596331e783dee6cd8fc3094b
HIPPER52 <hipper52@gmail.com>
2025-06-12 19:54:51 +0300

    course_project: moved RabbitMQ consumer logic to service

--- backend/cmd/consumer/main.go
+++ backend/cmd/consumer/main.go
@@ -5,9 +5,8 @@ import (
 	"course_project/cmd/consumer/handlers"
 	"course_project/internal/clients"
 	"course_project/internal/config"
-	"course_project/internal/constants/consumer"
+	"course_project/internal/consumer"
 	"course_project/internal/services/logger"
-	"encoding/json"
 	"fmt"
 )
 
@@ -24,32 +23,13 @@ func main() {
 		logger.Fatal(ctx, fmt.Errorf("clients init error: %w", err))
 	}
 
-	ch := clnts.RabbitMQ.Channel
-
-	msgs, err := ch.Consume(
-		cfg.RabbitMQQueue,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		logger.Fatal(ctx, fmt.Errorf("failed to register consumer: %w", err))
-	}
-
-	logger.Info(ctx, "Message consumer started...")
-
 	handler := handlers.NewHandler(clnts)
 
-	for d := range msgs {
-		var env consumer.Envelope
-		if err := json.Unmarshal(d.Body, &env); err != nil {
-			logger.Error(ctx, fmt.Errorf("failed to parse envelope: %w", err))
-			continue
-		}
+	cons := consumer.New(clnts.RabbitMQ.Channel, cfg.RabbitMQQueue, handler)
 
-		handler.Handle(ctx, env)
+	if err := cons.Start(ctx); err != nil {
+		logger.Fatal(ctx, fmt.Errorf("consumer start error: %w", err))
 	}
+
+	select {}
 }
